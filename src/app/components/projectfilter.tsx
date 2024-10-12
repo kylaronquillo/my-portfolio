cc4d8b74507419c883aa471851d1bd5e21f351d5
Kyla Ronquillo <kylasbronquillo@gmail.com>
2024-10-12 17:19:40 +0800

    fixed more bugs...

--- src/app/components/projectfilter.tsx
+++ src/app/components/projectfilter.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProjectTag = ({ name, onClick, isSelected }) => {
+const ProjectTag = ({ name, onClick }) => { // Removed isSelected
   return (
     <button
       className={`mt-10 bg-[#6EA0BA] text-white py-2 px-4 rounded-lg drop-shadow-xl hover:bg-white hover:text-[#6EA0BA] hover:border-[#6EA0BA] border-2 transition-all`}

c3260a95a3a4ca1046c8d972794be44c7f89d6d3
root <root@LAPTOP-K1GRSANR>
2024-10-14 11:19:26 +0800

    Add: Head Title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,39 @@
 "use client";
 
-import Projects from './components/projectsection';// Adjust the path as needed
+import Projects from './components/projectsection'; // Adjust the path as needed
 import Contact from './components/contact';
 import Header from './components/header';
 import About from './components/about';
+import Head from 'next/head'; // Import Head from next/head
 
 export default function Home() {
   return (
-    <div className="homepage bg-gray-50 min-h-screen flex flex-col items-center">
-      {/* Header with Navigation */}
-      <Header />
+    <>
+      <Head>
+        <title>Kyla Ronquillo</title>
+        <meta name="description" content="Your description here" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+      <div className="homepage bg-gray-50 min-h-screen flex flex-col items-center">
+        {/* Header with Navigation */}
+        <Header />
 
-      {/* About Section */}
-      <section id="about" className="about pb-10">        
-       <About />
-      </section>
-      
-      {/* Call the Projects Component */}
-      <section id="projects" className="projects pt-10 pb-10">
-        <Projects />
-      </section>
-      
-      {/* Contact Section */}
-      <section id="contact" className="contact pt-10 pb-10 drop-shadow">
-        <Contact />
-      </section>
-                      
-    </div>
+        {/* About Section */}
+        <section id="about" className="about pb-10">        
+          <About />
+        </section>
+        
+        {/* Projects Section */}
+        <section id="projects" className="projects pt-10 pb-10">
+          <Projects />
+        </section>
+        
+        {/* Contact Section */}
+        <section id="contact" className="contact pt-10 pb-10 drop-shadow">
+          <Contact />
+        </section>
+      </div>
+    </>
   );
 }
